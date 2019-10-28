33d1d04702e2837f669ebc9507b6539cf390d185
taiqui <chautaiqui@gmail.com>
2019-10-28 13:05:58 +0700

    third

--- shoppingcart.sql
+++ shoppingcart.sql
@@ -15,4 +15,11 @@ INSERT INTO `products` (`id`, `name`, `desc`, `price`, `rrp`, `quantity`, `img`,
 (2, 'Dalmore', '', '149.99', '0.00', 34, 'Dalmore.jpg', '2019-10-28 18:52:49'),
 (3, 'Hennessy XO', '', '79.99', '0.00', 23, 'hennessy_1.jpg', '2019-10-28 18:47:56'),
 (4, 'Hennessy VSOP', '', '69.99', '0.00', 7, 'hennessy_2.jpg', '2019-10-28 17:42:04'),
-(5, 'Royal Salute', '', '139.99', '0.00', 7, 'RoyalSalute.jpg', '2019-10-28 17:42:04');
+(5, 'Royal Salute', '', '139.99', '0.00', 7, 'RoyalSalute.jpg', '2019-10-28 17:45:04'),
+(6, 'Fantini Collection Red Blend', '', '99.99', '0.00', 7, 'Fantini.jpg', '2019-10-28 17:466:04'),
+(7, 'Amarone della Valpolicella', '', '129.99', '0.00', 7, 'Amarone.jpg', '2019-10-28 17:42:44'),
+(8, 'Roggio Velenosi', '', '69.99', '0.00', 7, 'Roggio.jpg', '2019-10-28 17:42:14'),
+(9, 'Vindoro', '', '59.99', '0.00', 7, 'Vindoro.jpg', '2019-10-28 17:52:14'),
+(10, 'Oreno 2015 Toscana', '', '89.99', '0.00', 7, 'Oreno.jpg', '2019-10-28 17:52:14'),
+(11, 'Vietti Barolo Castiglione', '', '79.99', '0.00', 7, 'Vietti.jpg', '2019-10-28 17:52:14'),
+(12, '50 Anniversario', '', '59.99', '0.00', 7, '50anniversario.jpg', '2019-10-28 17:52:14');
